57b27e2ae09be2c163c8fadea8c14243dbbef963
Brian Richter <brian@aviary.tech>
2025-09-23 12:41:42 -0700

    feat: Update Astro configuration for server and preview hosting options

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 4321
+  },
+  preview: {
+    host: true,
+    port: 4321
+  },
   vite: {
     server: {
       host: true,
