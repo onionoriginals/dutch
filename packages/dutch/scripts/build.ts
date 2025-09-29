da48fa0e5aa3e5d6fd6fbc1c1103fd8d6922d640
Brian Richter <brian@aviary.tech>
2025-09-28 20:42:21 -0700

    chore: Update build process to use Bun and add SQLite to .gitignore

---
+++ packages/dutch/scripts/build.ts
@@ -0,0 +1,24 @@
+import { $ } from 'bun'
+
+async function run() {
+  const externals = [
+    'bun:sqlite',
+    'bip39',
+    'bip32',
+    'tiny-secp256k1',
+    'bitcoinjs-lib',
+  ]
+
+  const externalFlags = externals.flatMap((dep) => ['-e', dep])
+
+  await $`bun build src/index.ts --bundle --format=cjs --target=node ${externalFlags} --outfile=dist/index.cjs`
+
+  await $`bun build src/browser.ts --bundle --format=esm --target=browser --outfile=dist/browser.js`
+}
+
+run().catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
+
+
